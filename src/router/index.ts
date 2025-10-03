01aef323d3a965316bb9d5481e200d55905bc945
jitbjony <jitbjony@gmail.com>
2025-10-03 22:16:34 +0600

    feat: routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,21 @@
+import LandingPage from '@/layouts/LandingPage.vue'
+import Home from '@/views/Home.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      component: LandingPage,
+      children: [
+        {
+          path: '/',
+          component: Home,
+        },
+      ],
+    },
+  ],
 })
 
 export default router
