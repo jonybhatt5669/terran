a37950473dd46f1d20926f4e940e12d700b30122
jitbjony <jitbjony@gmail.com>
2025-10-03 22:15:41 +0600

    Render all children

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1 class="text-5xl text-green-500">You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <router-view></router-view>
 </template>
 
 <style scoped></style>
