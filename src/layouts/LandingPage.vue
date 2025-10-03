84434f62ccdd9fec4ce251a3e2088638fba13f74
jitbjony <jitbjony@gmail.com>
2025-10-03 22:15:26 +0600

    Landing pagelayout

---
+++ src/layouts/LandingPage.vue
@@ -0,0 +1,14 @@
+<script setup>
+import Navbar from '@/components/shared/Navbar.vue'
+import { RouterView } from 'vue-router'
+</script>
+
+<template>
+  <main class="min-h-svh flex flex-col">
+    <Navbar />
+    <section class="flex-1">
+      <RouterView />
+    </section>
+    <p>Footer</p>
+  </main>
+</template>
