67380a83893c64380c189aabee87e5a6dcbcccd3
jitbjony <jitbjony@gmail.com>
2025-10-03 22:16:23 +0600

    UI: navbar

---
+++ src/components/shared/Navbar.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <nav class="py-6 px-10 flex items-center justify-between">
+    <!-- Left Navlinks -->
+    <RouterLink to="/t1pro">
+      <h3 class="font-medium text-lg">T1Pro</h3>
+    </RouterLink>
+  </nav>
+</template>
